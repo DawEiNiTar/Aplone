44dfe925c546e31341515779cc8dca5a2fdc913a
DawEiNiTar <57011173+DawEiNiTar@users.noreply.github.com>
2019-11-03 04:44:49 +0000

    Create register.php

---
+++ register.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="YUNLA APP">
+	  <meta name="keywords" content="www.yunla.com">
+  	<meta name="author" content="လဝင္​းထြန္​း ">
+<meta property="og:image" content="A.jpg" />
+
+    <link rel="stylesheet" type="text/css" href="./styles/style.css">
+      <title>​</title>
+      <style>
+      body{ font-size:12px; background-color:coral;	}
+      				
+    
+      a{text-decoration:none; color:white;}
+      
+      
+      section{font-size:8px; color:white;}
+      
+      input[type="text"],input[type="password"] { border:dashed 0.01px white; background:none;  margin-bottom:4px; border-left:none; border-right:none; border-top:none; margin-right:auto; margin-left:auto; width:40%; display:block;}
+      
+      #back{ display:block; margin-left:auto; margin-right:auto; width:30%; border:dotted 1px white; margin-top:40px; padding-top:10px; padding-bottom:10px; }
+      
+      
+      ::placeholder{color:white; text-align:center;}
+      
+      ::-webkit-input-placeholder{color:white; text-align:center;text-shadow:2px 1px 0px red; }
+      
+      :-ms-input-placeholder{color:white; text-align:center;}
+      
+      #sub{ margin-right:auto; margin-left:auto; width:40%; display:block;}
+      input[type="submit"]{width:100%; height:30px; background:none; border:dotted 1px white; color:white;}
+     
+     input[type="text"]:focus,input[type="password"]:focus{color:White; font-size:20px; text-shadow:2px 1px 0px red;}
+     
+</style>
+<body>
+
+
+<div id="back">
+ <section id="img">
+<center><a href="index.html">Click here to go back</a></center>
+</div>
+<br>
+<br>
+
+<div id="fill">
+<form action="register.php" method="POST"> 
+<br>
+<br>
+<input type="text" name="username" required="required" placeholder="Enter User name" /> 
+<br>
+<br>
+<input type="password" name="password" required="required" placeholder="Enter Password" /> 
+
+<br>
+
+<div id="sub">
+
+<input type="submit" value="Register"/> 
+
+</div>
+
+</form> 
+
+</div>
+
+</section>
+</div>
+
+</body>
+</html>
